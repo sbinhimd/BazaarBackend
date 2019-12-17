d44c12a629238936705a88e3e204a960b3fd112e
Suhaib Binhimd <sbinhimd@hotmail.com>
2019-12-17 15:05:19 +0300

    user img string

--- model/user.js
+++ model/user.js
@@ -30,7 +30,7 @@ const userSchema  = new Schema({
     profileimg:{
         default: "",
         required: false , 
-        type: Text
+        type: String
     },
     phonenumber:{
         required: true , 
