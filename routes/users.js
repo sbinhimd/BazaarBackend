c617848d3c9ae3230d64d817875432d30b20cf66
Suhaib Binhimd <sbinhimd@hotmail.com>
2019-12-19 00:06:42 +0300

    more populate

--- routes/users.js
+++ routes/users.js
@@ -58,7 +58,7 @@ User.findOne({email: req.body.email})
 /* GET one user . */
 router.get('/:id' ,passport.authenticate('jwt', {session: false}), async(req, res, next) =>{
   try {
-    var result = await User.findById(req.params.id).populate('following','firstname lastname username profileimg Rating').populate('followers','firstname lastname username profileimg Rating').populate('purchesedorder','description postimages city').populate('posts').populate('comments').populate('watchlater','title description postimages city').populate('msg');
+    var result = await User.findById(req.params.id).populate('following','firstname lastname username profileimg Rating').populate('followers','firstname lastname username profileimg Rating').populate('purchesedorder','title description postimages city createdAt').populate('posts').populate('comments').populate('watchlater','title description postimages city').populate('msg');
     res.send({result});
 } catch (error) {
     res.send({error})
